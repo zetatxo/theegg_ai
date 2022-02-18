725a8e0c999c8108292688b6669e89eb2359e96a
Ziortza <ziortzaispizua@hotmail.com>
2022-02-18 21:44:37 +0100

    Subo tarea 94

---
+++ tarea_94/calculadora.py
@@ -0,0 +1,35 @@
+"""Módulo que definirá suma, resta, producto y division"""
+
+def sumar(a,b):
+    try:
+        n1 = float(a)
+        n2 = float(b)
+        print(n1 + n2)
+    except ValueError:
+        print("Debe introducir números para ejecutar la función")
+    
+def restar (a,b):
+    try:
+        n1 = float(a)
+        n2 = float(b)
+        print(n1 - n2)
+    except ValueError:
+        print("Debe introducir números para ejecutar la función")
+
+def multiplicar (a,b):
+    try:
+        n1 = float(a)
+        n2 = float(b)
+        print(n1 * n2)
+    except ValueError:
+        print("Debe introducir números para ejecutar la función")
+
+def dividir (a,b):
+    try:
+        n1 = float(a)
+        n2 = float(b)
+        print(n1 / n2)
+    except ValueError:
+        print("Debe introducir números para ejecutar la función")
+    except ZeroDivisionError:
+        print("No es posible dividir por 0")
---
+++ tarea_94/calculos.py
@@ -0,0 +1,14 @@
+import calculadora as calc
+
+calc.sumar("a", 5)
+calc.sumar(5, 4)
+
+calc.restar("!",5)
+calc.restar(6, 4)
+
+calc.multiplicar("r", 3)
+calc.multiplicar(3, 5)
+
+calc.dividir("s", 3)
+calc.dividir(5, 0)
+calc.dividir (10, 2)
